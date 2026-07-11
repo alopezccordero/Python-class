ffffd86a765c68ba7776bbb7a32c29c06e760607
alopezcordero <alejandro.lopezcordero01@utrgv.edu>
2026-07-10 20:40:21 -0500

    copied humanoid_direction

--- mujoco/humanoid_direction/humanoid_direction_evaluate.py
+++ mujoco/humanoid_direction/humanoid_direction_evaluate.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 import register_env
 from stable_baselines3 import PPO
-model = PPO.load("models/ppo_humanoid_direction_20m.zip", device="cpu")
+model = PPO.load("models/ppo_humanoid_direction_real_amp_filtered.ZIP", device="cpu")
 env = gym.make("HumanoidDirection-v0", render_mode="human")
 
 obs, info = env.reset()
@@ -9,12 +9,12 @@ obs, info = env.reset()
 episode_reward = 0
 
 episode = 0
-num_episode = 5
+num_episode = 10000
 
 while episode < num_episode:
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, terminated, truncated, info = env.step(action)
-
+    env.render()
     episode_reward += reward
     
     if terminated or truncated:
